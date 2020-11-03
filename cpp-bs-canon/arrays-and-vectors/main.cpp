9b4dd8bec680a741e1cf761d386e3d6ea93b61ba
Krystian Piękoś <krystian.piekos@infotraining.pl>
2020-11-03 14:09:57 +0100

    Arrays & vectors

---
+++ cpp-bs-canon/arrays-and-vectors/main.cpp
@@ -0,0 +1,125 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+namespace CStyle
+{
+	void fill_tab(int* tab, size_t size)
+	{
+		int value = 0;
+		for (int* it = tab; it != tab + size; ++it)
+			*it = ++value;
+	}
+}
+
+namespace Step1
+{
+	template <typename T, size_t N>
+	void fill_tab(T(&tab)[N])
+	{
+		T value{};
+		for (T& item : tab)
+			item = ++value;
+	}
+}
+
+template <typename TContainer>
+void fill_container(TContainer& container)
+{
+	using T = std::decay_t<decltype(container[0])>;
+	
+	T value{};
+
+	for (T& item : container)
+		item = ++value;
+}
+
+template <typename T>
+void print(const T& container)
+{
+	for (auto item : container)
+		std::cout << item << " ";
+	std::cout << "\n";
+}
+
+
+
+TEST_CASE("native arrays - old style")
+{
+	constexpr int size = 20;
+	int tab1[size] = {1, 2, 3};
+
+	tab1[5] = 6;
+
+	//CStyle::fill_tab(tab1, 10);
+	std::iota(std::begin(tab1), std::end(tab1), 1);
+	print(tab1);
+
+	double dbls[10];
+
+	//fill_container(dbls);
+	std::fill(std::begin(dbls), std::end(dbls), 3.14);
+	print(dbls);
+
+	std::string text = "text";
+	print(text);
+}
+
+TEST_CASE("std::array - modern")
+{
+	std::array<int, 10> arr1 = { 1, 2, 3 };
+
+	arr1[5] = 6;
+
+	print(arr1);
+
+	REQUIRE(arr1.size() == 10);
+
+	arr1.fill(-1);
+}
+
+TEST_CASE("dynamic arrays - old style")
+{
+	int size = 10;
+	int* tab = new int[size];
+
+	for (int i = 0; i < size; ++i)
+		tab[i] = i;
+
+	delete[] tab;
+}
+
+TEST_CASE("vector - modern")
+{
+	std::vector<int> vec(10);
+
+	vec.resize(30);
+
+	fill_container(vec);
+
+	print(vec);
+}
+
+TEST_CASE("multidimensional vector")
+{
+	using Matrix = std::vector<std::vector<int>>;
+
+	Matrix matrix = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+
+	matrix[1][1] = 0;
+
+	REQUIRE(matrix[1].size() == 3);
+
+	for (const auto& row : matrix)
+	{
+		for (const auto& item : row)
+		{
+			std::cout << item << " ";
+		}
+		std::cout << "\n";
+	}
+}
--- cpp-bs-canon/basic-types/main.cpp
+++ cpp-bs-canon/basic-types/main.cpp
@@ -1,8 +1,68 @@
 #include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std::literals;
 
 int main()
 {
-	std::cout << "Hello world!!!\n";
+	char c = -65;
+	unsigned char uc{ 65 };
 
-	std::cout << "C++ szkolenie\n";
+	std::cout << static_cast<int>(uc) << "\n";
+
+	short s = 1024;
+	unsigned short us{ 887 };
+
+	int x = 42;
+	unsigned int ux = 665;
+
+	long l{ 7'656'576 };
+	unsigned long ul = 1'000'000'888UL;
+
+	int8_t x1 = 66;
+	uint8_t x2 = 87;
+
+	int16_t x3 = 76;
+	int32_t x4 = 665;
+	uint64_t x5 = 1'000'000ULL;
+
+	// auto + number literal
+	auto ax1 = 42; // int ax1
+	auto ax2 = 'a'; // char ax2
+	auto ax3 = 42L; // long ax3
+	auto ax4 = 1'000'000ULL; // unsigned long long
+
+	// floating points
+	float pi_f = 3.14F;
+	double pi_d = 3.14;
+	auto pi = 3.14;
+
+	// bool
+	bool flag = true;
+	flag = false;
+
+	flag = s; // s = true - implicit conversion from integral type to bool
+
+	// c-string
+	const char* str1 = "text";
+	auto astr1 = "text"; // const char*
+	assert(strlen(str1) == 4);
+	// str1[0] = 'T'; // illegal
+
+	const char* another = "Text";
+	assert(strcmp(str1, another) == 1);
+
+	// std::string
+	std::string str2 = "text";
+	auto astr2 = "text"s;
+	str2[0] = 'T';
+
+	assert(str2.size() == 4);
+	assert(str2 == "Text"s);
+	assert("text"s > str2);
+
+	str2 += "!!!";
+
+	std::cout << str2 << "\n";
 }
