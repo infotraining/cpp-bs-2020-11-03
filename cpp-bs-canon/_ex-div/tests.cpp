31b94737f28d33913ab6fba629876f515d3c50ad
Krystian Piękoś <krystian.piekos@infotraining.pl>
2020-11-03 16:00:04 +0100

    div - solution

--- cpp-bs-canon/_ex-div/tests.cpp
+++ cpp-bs-canon/_ex-div/tests.cpp
@@ -3,10 +3,25 @@
 #include <numeric>
 #include <string>
 #include <vector>
+#include <tuple>
 
 #include "catch.hpp"
 
 using namespace std;
+
+void divide(int x, int y, int& quotient_inout, int& reminder_inout)
+{
+    quotient_inout = x / y;
+    reminder_inout = x % y;
+}
+
+namespace Cpp17
+{
+    auto divide(int x, int y)
+    {
+        return std::tuple(x / y, x % y);
+    }
+}
 
 TEST_CASE("div")
 {
@@ -14,8 +29,20 @@ TEST_CASE("div")
     int y = 2;
 
     int quotient, reminder;
+    divide(x, y, quotient, reminder);
+    
+    cout << quotient << "  " << reminder << endl;
 
-    div(x, y, quotient, reminder);
+    REQUIRE(quotient == 2);
+    REQUIRE(reminder == 1);
+}
+
+TEST_CASE("div - c++17")
+{
+    int x = 5;
+    int y = 2;
+
+    auto [quotient, reminder] = Cpp17::divide(x, y); // structured bindings
 
     REQUIRE(quotient == 2);
     REQUIRE(reminder == 1);
