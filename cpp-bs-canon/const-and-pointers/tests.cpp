a90217cb93f3db1075b2d20b910485ec1d62493c
Krystian Piękoś <krystian.piekos@infotraining.pl>
2020-11-04 10:32:52 +0100

    const & pointers

--- cpp-bs-canon/const-and-pointers/tests.cpp
+++ cpp-bs-canon/const-and-pointers/tests.cpp
@@ -8,15 +8,52 @@
 
 using namespace std;
 
-TEST_CASE("div")
+TEST_CASE("const")
 {
-    int x = 5;
-    int y = 2;
+	const int x = 42;
+	const int& cref_x = x;
+}
 
-    int quotient, reminder;
+TEST_CASE("const + pointers")
+{
+	SECTION("pointer to const")
+	{
+		int x = 42;
+		int y = 1024;
 
-    div(x, y, quotient, reminder);
+		const int* ptr_to_const = &x;
 
-    REQUIRE(quotient == 2);
-    REQUIRE(reminder == 1);
+		//*ptr_to_const = 665; // ERROR
+
+		ptr_to_const = &y;
+
+		REQUIRE(*ptr_to_const == 1024);
+
+		const std::string name = "Jan";
+
+		const std::string* ptr = &name;
+		const std::string& ref = name;
+	}
+
+	SECTION("const pointer")
+	{
+		int x = 42;
+		int y = 1024;
+
+		int* const const_ptr = &x;
+
+		*const_ptr = 665; // OK
+		//const_ptr = &y; // ERROR
+	}
+
+	SECTION("const pointer to const")
+	{
+		int x = 42;
+		int y = 1024;
+
+		const int* const const_ptr_to_const = &x;
+
+		//*const_ptr_to_const = 665; // ERROR
+		//const_ptr_to_const = &y; // ERROR
+	}
 }
