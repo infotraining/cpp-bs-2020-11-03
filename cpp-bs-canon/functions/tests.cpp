27f0add3b9e9db5b328f686ba445b78441bb8834
Krystian Piękoś <krystian.piekos@infotraining.pl>
2020-11-04 09:58:27 +0100

    auto + decltype(auto)

--- cpp-bs-canon/functions/tests.cpp
+++ cpp-bs-canon/functions/tests.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 using namespace Catch::Matchers;
@@ -124,4 +125,68 @@ TEST_CASE("functions - passing arguments")
 
     InOutParam::increment(x);
     REQUIRE(x == 44);
+
+    print(vec); // passing lvalue
+    print(std::vector<int>{1, 2, 3, 4}); // passing temporary vector - rvalue
+}
+
+std::string full_name(const std::string& first, const std::string& last)
+{
+    return first + " " + last;
+}
+
+std::vector<int> create_vector(size_t size)
+{
+    std::vector<int> vec(size);
+
+    std::iota(vec.begin(), vec.end(), 1);
+
+    return vec;
+}
+
+decltype(auto) value()
+{
+    static int x = 42;
+    int& ref_x = x;
+    return ref_x;
+}
+
+TEST_CASE("returning values from functions")
+{
+    std::string person = full_name("Jan", "Kowalski");
+
+    REQUIRE(person == "Jan Kowalski"s);
+
+    std::vector<int> data = create_vector(1'000'000);
+
+    int& ref_value = value();
+
+    std::cout << ref_value << "\n";
+}
+
+template <typename T>
+auto add(const T& x, const T& y)
+{
+    return x + y;
+}
+
+template <typename T>
+auto multiply(const T& x, const T& y)
+{
+    return x * y;
+}
+
+auto describe(int x)
+{
+    if (x % 2 == 0)
+        return std::string("Even");
+    else
+        return "Odd"s;
+}
+
+TEST_CASE("auto as return type")
+{
+    auto result = add(1, 5); // int result
+
+    auto word = add("one"s, "two"s); // std::string word
 }
