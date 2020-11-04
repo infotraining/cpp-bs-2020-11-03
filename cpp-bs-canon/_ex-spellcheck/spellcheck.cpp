9be58d99c34317ef6a4d45a58a09f9904caf2dce
Krystian Piękoś <krystian.piekos@infotraining.pl>
2020-11-04 16:07:52 +0100

    spellcheck - solution

--- cpp-bs-canon/_ex-spellcheck/spellcheck.cpp
+++ cpp-bs-canon/_ex-spellcheck/spellcheck.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 #include <regex>
+#include <unordered_set>
 
 using namespace std;
 
@@ -31,7 +32,58 @@ int main()
     // wczytaj zawartość pliku en.dict ("słownik języka angielskiego")    
     // sprawdź poprawość pisowni następującego zdania:    
     string input_text = "this is an exmple of very badd snetence";
+
     vector<string> words = regex_split(input_text);
 
-    // TODO
+    ifstream in("en.dict");
+
+    if (!in)
+    {
+        std::exit(1);
+    }
+
+
+    std::unordered_set<std::string> dict;
+    std::string entry;
+
+    while (in >> entry)
+    {
+        dict.insert(entry);
+    }
+
+    std::cout << "Dictionary size: " << dict.size() << "\n";
+    
+    std::vector<std::string> misspelled;
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (const auto& tocheck : words)
+        if (dict.find(tocheck) == dict.end())
+            misspelled.push_back(tocheck);
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    for(const auto& item : misspelled)
+        cout << item << " - " << "not exist" << endl;
+
+    std::cout << "Time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us\n\n";
+
+    /////////////////////////////////////////////////////////////////
+
+    std::vector<std::string> vec_dict(dict.begin(), dict.end());
+    std::sort(vec_dict.begin(), vec_dict.end());
+    misspelled.clear();
+
+    start = std::chrono::high_resolution_clock::now();
+
+    for (const auto& tocheck : words)
+        if (!std::binary_search(vec_dict.begin(), vec_dict.end(), tocheck))
+            misspelled.push_back(tocheck);
+
+    end = std::chrono::high_resolution_clock::now();
+
+    for (const auto& item : misspelled)
+        cout << item << " - " << "not exist" << endl;
+
+    std::cout << "Time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us\n";
 }
