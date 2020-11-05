420be324a00a958b11ea5764551be41be9946a34
Krystian Piękoś <krystian.piekos@infotraining.pl>
2020-11-05 10:38:14 +0100

    oop

---
+++ cpp-bs-canon/oop/tests.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+#include <list>
+#include <execution>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_map>
+
+#include "catch.hpp"
+
+using namespace std;
+
+TEST_CASE("")
+{
+}
