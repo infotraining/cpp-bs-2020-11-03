89ef328c6559e88514c76529818a01567fb2838b
Krystian Piękoś <krystian.piekos@infotraining.pl>
2020-11-06 12:52:51 +0100

    lambda

---
+++ cpp-bs-canon/lamba-expressions/tests.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+#include <tuple>
+
+#include "catch.hpp"
+
+using namespace std;
+
+TEST_CASE("lambda")
+{
+}
